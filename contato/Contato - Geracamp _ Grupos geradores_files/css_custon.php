0e57456233d8be008d0bbca9c52e451f4df36f99
Caique <cai.fs@hotmail.com>
2021-09-11 10:54:46 -0300

    Inicializando

---
+++ contato/Contato - Geracamp _ Grupos geradores_files/css_custon.php
@@ -0,0 +1,20 @@
+.c-layout-header .c-topbar-light .c-top-menu > .c-icons > li > a:not(.btn) > i { color: #FFF; }
+.c-layout-header .c-topbar-light .c-top-menu > .c-links > li > a:not(.btn){ color: #FFF;}
+.c-page-on-scroll .c-layout-header.c-layout-header-2 {
+    background: rgba(0, 50, 95, 0.79);}
+@media (max-width: 991px){
+.c-layout-header.c-layout-header-dark-mobile {
+    background: #002b59;}
+.c-layout-header.c-layout-header-dark-mobile .c-brand > .c-hor-nav-toggler {
+    top: 12px;
+    display: inline-block;
+    background: #1e1e1f;
+    padding: 10px 13px 10px 13px;
+}
+.c-layout-header.c-layout-header-dark-mobile .c-brand > .c-hor-nav-toggler > .c-line {
+    background: #ffffff;
+}
+.c-layout-header-topbar-collapse .c-layout-header .c-brand > .c-topbar-toggler {
+    display: none;
+}
+}
